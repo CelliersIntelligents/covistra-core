9c4eca2e58218e6098a317f6106b6ad25abf9828
joel Grenon <joelgrenon@covistra.com>
2016-01-28 22:24:00 -0500

    Apply Apache license to cmbf-core

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
-
- Copyright 2015 Covistra Technologies Inc.
+ Copyright 2015-2016 Covistra Technologies Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
